16166333723bfc868714a5d4e82479b7effd131e
P S <PS@P-Ss-MacBook-Pro.local>
2017-02-28 10:55:55 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/search', function (req, res) {
+    res.render('search');
+});
+
+var port_number = process.env.PORT || 3000;
+
+app.listen(port_number, function () {
+  console.log('Movie Search Site on Port: ' + port_number);
+});
