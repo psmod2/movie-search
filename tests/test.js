38b5c86d0234467910397cb0f613fd9612bf669b
P S <PS@P-Ss-MacBook-Pro.local>
2017-03-01 16:09:52 +0000

    Update

--- tests/test.js
+++ tests/test.js
@@ -5,9 +5,19 @@ var serverFile = require('../server.js');
 
 
   describe('getting data from api', function() {
-    it('searchMovieAPI() should return nothing path and query parameters empty', function() {
+    it('searchMovieAPI() should return home/empty path and query parameters empty', function() {
       return serverFile.searchMovieAPI('', '').then(function(data){
           expect(data.url).to.equal('/');
       })
     })
   });
+
+  describe('getting data from api', function() {
+    it('searchMovieAPI() should return undefined and query parameters null', function() {
+      return serverFile.searchMovieAPI(null, null).then(function(data){
+          expect(data.url).to.equal(undefined);
+      })
+    })
+  });
+
+  
